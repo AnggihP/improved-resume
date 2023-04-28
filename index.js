b5e87e71a387fd1c3aafcce553e59d69fcf9f68b
Anggih Pratama <anggih10@gmail.com>
2023-04-28 13:50:40 +0700

    fixing toggle and change photo

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ app.use('/', router);
 app.listen(port, function () {
     console.log('Listening on port ' + port + '...');
 });
+
+function toggleMobileMenu() {
+    var e = document.getElementById("m-menu-button")
+      , o = document.getElementById("m-menu");
+    e.children[1].classList.contains("block") ? (e.children[1].classList.replace("block", "hidden"),
+    e.children[2].classList.replace("hidden", "block"),
+    o.classList.replace("hidden", "block")) : (e.children[1].classList.replace("hidden", "block"),
+    e.children[2].classList.replace("block", "hidden"),
+    o.classList.replace("block", "hidden"))
+}
